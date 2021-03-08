6b6c3475143c384cacbff0becdfcb107577a8641
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2021-03-08 23:29:02 +0900

    サイトと2020/10-12,2021/1の時刻データを追加(レポジトリ再構成)

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# sist_bus
-GitHub Pagesの大学バス予定表示ツール
+# GitHub Pages
+大学のバスの時刻をGitHub Pagesで表示しています。<br>
+https://s-n-1-0.github.io/sist_bus/
---
+++ docs/unc/get_schedule.js
@@ -0,0 +1,72 @@
+function GetSchedule(yyyy, MM,dd,day,ex, fgot) {
+    var mode = -1;
+    /*土日ではないか*/
+    if(day != 0 && day !=6){
+        mode = 0;
+    }
+    /*特別運行・特別運休かどうか*/
+    if(ex != null){
+
+        for (idx in ex){
+           let e = ex[idx];
+          
+          if (e["dd"] == dd){
+              
+              mode = e["exception"];
+              break;
+          }
+        }
+    }
+
+
+    if(mode == -1){//最終モードが-1なら取得しない(運休)
+        fgot(mode,null);
+        return;
+    }
+    $.ajax({
+        url: "./unc/json/schedule/" + String(yyyy) + "_" + String(MM) + ".json",
+        dataType: 'json',
+        success: function( data ) {
+            var sm2 = data["data"];
+            for (var i in sm2) {
+                if (sm2[i]["mode"] == '0') {
+                    fgot(mode, sm2[i]);
+                    return;
+                }
+            }
+        },
+        error: function() {
+            fgot(-1, null);
+        }
+      });
+}
+function GetScheduleEx(yyyy, MM, fgot) {
+    $.ajax({
+        url: "./unc/json/schedule/" + String(yyyy) + "_" + String(MM) + ".json",
+        dataType: 'json',
+        success: function( data ) {
+            var sm2 = data["ex"];
+            for (i in sm2){
+                sm2[i]['comment'] = '';
+                switch(sm2[i]['exception']){
+                case -2:
+                sm2[i]['comment'] = "運休" //確実に運休の場合
+                case -1:
+                sm2[i]['comment'] = "意図的なデータ未入力(PDF見てください)";
+                break;
+                case 0:
+                sm2[i]['comment'] = "通常運行(本来は運休)";
+                break;
+                default:
+                sm2[i]['comment'] = "特別運行";
+                break;
+                }
+            }
+                    fgot(sm2);
+                    return;
+        },
+        error: function() {
+            fgot(null);
+        }
+      });
+}
