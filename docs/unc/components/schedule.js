dbbeaa8f7c2e6330879d1ec8bf6ef382a00031df
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2021-09-21 02:04:41 +0900

    Vueコンポーネントとして時刻表(リスト)を分離

---
+++ docs/unc/components/schedule.js
@@ -0,0 +1,67 @@
+Vue.component('schedule-irregular',{
+  props:['schedule'],
+template:`
+<div class="text-center">
+<b>変則運転予定表</b>
+<div>
+  <table>
+    <thead>
+      <tr>
+        <th>設定されている変則運転または運休日(土日を除く)</th>
+        <th> </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in schedule.schedule_ex">
+        <td><b>{{row.dd}}日</b></td>
+        <td>{{row.comment}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>`
+})
+Vue.component('schedule-times',{
+props:['toC','schedule'],
+template:`
+<div>
+<div class="text-center">
+<a class="introvert2" href="https://forms.office.com/Pages/ResponsePage.aspx?id=Bb0r1yPrmEGObOE4I--qiDTo6qug1SlGruWNB4fcjXxUNElKS1BBMU9WRUpQV1RPM0lGNzU5QzNKQS4u">時刻表示ミスがある場合の報告</a><br>
+</div>
+<div v-if="toC">
+  <table>
+    <thead>
+      <tr>
+        <th>時刻</th>
+        <th>駅発</th>
+        <th>大学着</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in schedule.schedule_c">
+        <td><b v-if="row.isShowHH">{{row.HH}}時</b></td>
+        <td>{{row.mm}}</td>
+        <td>{{row.arrival_mm}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+<div v-else="toC">
+  <table>
+    <thead>
+      <tr>
+        <th>時刻</th>
+        <th>大学発</th>
+        <th>駅着</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in schedule.schedule_a">
+        <td><b v-if="row.isShowHH">{{row.HH}}時</b></td>
+        <td>{{row.mm}}</td>
+        <td>{{row.arrival_mm}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div></div>`
+});
