cff6bc0c4c97e1ccd8909fc0f2e52faf0f5dbbf3
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-03-06 01:37:54 +0900

    webpackバンドル設定の追加

---
+++ core/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require("vue-loader");
+module.exports = {
+    entry: {
+        'index':"./src/index.ts",
+    },
+    output: {
+        path: path.join(__dirname, '../docs/bundle'),
+        filename: `[name].js`,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+          },
+          {
+            test: /\.css$/,
+            use: [
+              'vue-style-loader',
+              'css-loader',
+            ]
+          },
+          {
+            test: /\.ts$/,
+            loader: "ts-loader",
+            include: __dirname,
+            options:{
+              appendTsSuffixTo: [/\.vue$/]
+            }
+          }
+        ]
+    },
+    resolve:{
+        alias:{
+        },
+        extensions: [".js", ".vue",".ts"]
+    },
+    externals: {
+    },
+    plugins: [new VueLoaderPlugin(),
+              new webpack.DefinePlugin({
+                __VUE_PROD_DEVTOOLS__: false
+              })]
+};
