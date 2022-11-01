a199d8e4047bebad246b46f14c55947eb5b868a2
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2022-11-01 14:23:30 +0900

    サイト更新メンバー募集モーダルの内容更新

--- core/src/components/recruitment-modal.vue
+++ core/src/components/recruitment-modal.vue
@@ -3,30 +3,31 @@
         <div class="modal-dialog modal-fullscreen">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">🚌バスツール管理者募集</h5>
+                <h5 class="modal-title">🚌バスツール運営募集</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body text-start">
-                バスツールの作成者である私が大学を離れた時点でツールは閉鎖されます。<br>
-                ツールの管理は全く難しくないのでもしバスツールが欲しい方いましたら下記リンクへ連絡ください。<br>
-                週100人前後が利用しているのでたぶん就活とかの学内貢献枠に書けるかもしれません。<br>
+                現在本サイトの運営メンバーを募集しています。<br>
+                やることは基本ブラウザ操作なので簡単です。もし興味がございましたら下記リンクへ連絡ください。<br>
+                週数百ユーザーが利用しているのでたぶん就活とかに書けるかもしれません。<br>
                 <br>
                 ちなみに管理にプログラミング知識は不要です。<br>
+                (もちろん知識があったらサイトを好きにいじれます)<br>
                 <br>
-                <h3>条件</h3>
+                <h3>募集条件や仕事内容</h3>
                 <ul>
-                    <li>2年生以下(学部学科問わない)</li>
-                    <li>月初めに時刻表の更新作業(5分程度のコピペ)をサボらずできる方</li>
+                    <li>募集 : 2年生以下(学部学科問わない)</li>
+                    <li>仕事内容 : 月初めにバスサイトの更新(5分程度のコピペ)</li>
                     <ul>
-                        <li style="list-style: circle;">更新作業が数ヶ月行われていない場合、こちらでサイトを閉鎖する場合があります。</li>
+                        <li style="list-style: circle;">長い間更新作業が行われていない場合、サイトが閉鎖される可能性があります。</li>
                     </ul>
                 </ul>
                 <br>
                 <h3>備考</h3>
                 <ul>
-                    <li>プログラミング知識不要(あると好きにサイトをいじれます)</li>
+                    <li>プログラミング知識不要(あるとサイトを好きにいじれます)</li>
                     <li>Googleアカウント必須</li>
-                    <li>GitHubアカウント必須(GitHubがわからない方は説明します)</li>
+                    <li>GitHubアカウント必須 (GitHubがわからない方は説明します)</li>
                 </ul>
                 <br>
                 <hr>
