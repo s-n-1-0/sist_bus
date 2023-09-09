c6e41f7b139a59412bf4137814daa438f32e5e4a
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-09-09 18:48:34 +0900

    アナウンス用コンポーネントを作成

---
+++ app/src/components/AnnouncementBlock.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="announce">
+    <p class="mb-0 pb-0">
+      <b>アナウンス(09/01)</b>
+    </p>
+    <div>
+      9月更新しました。<br />
+      オープンキャンパス(10日)は表示されませんが運行があります。
+    </div>
+  </div>
+</template>
