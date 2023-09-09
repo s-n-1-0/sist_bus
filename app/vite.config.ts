cb694a82398bae6e9d9480caf6513b69d5f0b9cf
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-09-09 18:13:31 +0900

    docsは自動生成するため削除

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,6 +7,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/sist_bus",
   plugins: [vue()],
+  build: {
+    outDir: "../docs",
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
