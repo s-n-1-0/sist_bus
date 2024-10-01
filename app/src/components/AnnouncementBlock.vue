56fdc2cbc5276d79f85f0790340e39ef83a40cb4
KANAYAMA <103913975+026KANAYAMA@users.noreply.github.com>
2024-10-01 17:46:22 +0900

    10月アナウンス更新

--- app/src/components/AnnouncementBlock.vue
+++ app/src/components/AnnouncementBlock.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="announce">
     <p class="mb-0 pb-0">
-      <b>アナウンス(9/02)</b>
+      <b>アナウンス(10/01)</b>
     </p>
     <div>
-      9月更新しました。<br />
+      10月更新しました。<br />
     </div>
   </div>
 </template>
